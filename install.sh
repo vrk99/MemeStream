340aeb305229929ca252fdfc16e1a38d66d65325
Virik Jain <vrkgajani@gmail.com>
2021-02-12 15:28:52 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt update
-sudo apt install mongodb-org
+sudo apt install -y mongodb-org
 sudo apt update
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install -y nodejs
+sudo apt install -y npm
 
 # Any configuration related commands
 
